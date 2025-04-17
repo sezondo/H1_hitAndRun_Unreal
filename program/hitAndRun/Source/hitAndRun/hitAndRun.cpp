c7c1e9622d79e79af27d86951e00ea8275e4dcb4
sezondo <psdd205050@gmail.com>
2025-04-18 00:27:38 +0900

    첫 움직임 구현 테스트 실패 에러 198개 떠서 프로그램 삭제후 나중에 재생성예정

--- program/hitAndRun/Source/hitAndRun/hitAndRun.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "hitAndRun.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, hitAndRun, "hitAndRun" );
