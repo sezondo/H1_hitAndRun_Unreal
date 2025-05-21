1a95e272c2ab1ee405b030f23b0cdced3c2f3208
sezondo <psdd205050@gmail.com>
2025-05-21 17:35:27 +0900

    곰애니메이션전달

---
+++ program/H1/Source/H1/H1BearBehaviorTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "H1BearBehaviorTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class H1_API UH1BearBehaviorTree : public UBehaviorTree
+{
+	GENERATED_BODY()
+	
+};
